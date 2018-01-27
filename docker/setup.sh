9a5fc4fa837a2d3ee6c2ca00007fcde8ca11964b
Alison Abreu-Garcia <alison.iag@gmail.com>
2018-01-27 17:38:24 -0500

    add mysql

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker network create -d bridge acornnet 2>/dev/null
+#docker network create -d bridge acornnet 2>/dev/null
 script_dir="${BASH_SOURCE%/*}" 1>/dev/null || exit 1
 
 symlink-compose-override() {
@@ -7,15 +7,4 @@ symlink-compose-override() {
   unlink $PWD/../docker-compose.override.yml 2>/dev/null
   ln -s $PWD/overrides/$1.yml $PWD/../docker-compose.override.yml
   popd 1>/dev/null
-}
-
-routine-checks() {
-  if [[ ! -f $location/../cms/config/database.yml ]]; then
-    echo "ERROR: database.yml missing! you need this before the server will start"
-    exit 1
-  fi
-  if [[ ! -f $location/../.env ]]; then
-    echo "ERROR: .env file missing! you need this before the server will start"
-    exit 1
-  fi
 }
